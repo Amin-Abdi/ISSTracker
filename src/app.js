b7244a0670e4a4c4b60b49b59f9511b9d8ec0e85
Amin <malmaaz78@gmail.com>
2021-04-02 22:06:40 +0100

    Implemented a setInterval Get request every second

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@
 var mymap = L.map("mapid", {
   center: [0, 0],
   zoom: 2.5,
-  // scrollWheelZoom: true,
 });
+// scrollWheelZoom: true,
 
 const leafAttribution =
   '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
@@ -26,6 +26,7 @@ const marker = L.marker([0, 0], { icon: myIcon }).addTo(mymap);
 
 const API_Url = "https://api.wheretheiss.at/v1/satellites/25544";
 
+//Getting the current location of the International space station
 async function getISSPosition() {
   const response = await fetch(API_Url);
   const data = await response.json();
@@ -35,21 +36,25 @@ async function getISSPosition() {
   //Adding data to the table
   document.querySelector(".id").textContent = id;
   document.querySelector(".name").textContent = name.toUpperCase();
-  document.querySelector(".latitude").textContent = latitude;
-  document.querySelector(".longitude").textContent = longitude;
-  document.querySelector(".altitude").textContent = altitude + " km";
-  document.querySelector(".velocity").textContent = velocity + " km/h";
+  document.querySelector(".latitude").textContent = latitude.toFixed(3);
+  document.querySelector(".longitude").textContent = longitude.toFixed(3);
+  document.querySelector(".altitude").textContent = altitude.toFixed(2) + " km";
+  document.querySelector(".velocity").textContent =
+    velocity.toFixed(2) + " km/h";
 
   mymap.setView([latitude, longitude]);
   // L.marker([latitude, longitude]).addTo(mymap);
   marker.setLatLng([latitude, longitude]);
 
-  console.log("id: ", id);
-  console.log("Lat: ", latitude);
-  console.log("Long: ", longitude);
-  console.log("name: ", name);
-  console.log("altitude: ", altitude);
-  console.log("Velocity: ", velocity);
+  // console.log("id: ", id);
+  // console.log("Lat: ", latitude);
+  // console.log("Long: ", longitude);
+  // console.log("name: ", name);
+  // console.log("altitude: ", altitude);
+  // console.log("Velocity: ", velocity);
 }
 
 getISSPosition();
+
+//Fetching data every
+setInterval(getISSPosition, 1000);
