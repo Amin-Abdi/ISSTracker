81d98c49a13075ab9b129440e682e4367261d74e
Amin <malmaaz78@gmail.com>
2021-04-02 19:28:58 +0100

    Added a marker mapped to the location of the ISS

--- src/app.js
+++ src/app.js
@@ -12,22 +12,27 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: leafAttribution,
 }).addTo(mymap);
 
-L.marker([0, 0]).addTo(mymap);
+//Initial position of marker
+const marker = L.marker([0, 0]).addTo(mymap);
 
 const API_Url = "https://api.wheretheiss.at/v1/satellites/25544";
 
 async function getISSPosition() {
   const response = await fetch(API_Url);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   const { id, latitude, longitude, name, altitude, velocity } = data;
 
+  //Getting the name, id, latitude, longitude, altitude, velocity from the data
   document.querySelector(".id").textContent = id;
   document.querySelector(".name").textContent = name.toUpperCase();
   document.querySelector(".latitude").textContent = latitude;
   document.querySelector(".longitude").textContent = longitude;
   document.querySelector(".altitude").textContent = altitude;
   document.querySelector(".velocity").textContent = velocity;
+
+  // L.marker([latitude, longitude]).addTo(mymap);
+  marker.setLatLng([latitude, longitude]);
 
   console.log("id: ", id);
   console.log("Lat: ", latitude);
